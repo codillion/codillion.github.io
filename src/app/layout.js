4a2aea3b40e47208b038c758f27171311c3f42d1
CODILLION <163315001+codillion@users.noreply.github.com>
2024-03-17 06:27:51 +0000

    toploader

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import NextTopLoader from 'nextjs-toploader';
+
 export const metadata = {
   title: "CODILLION - BORN TO CODE",
   description: "OFFICIAL WEBSITE OF CODILLION",
@@ -8,7 +10,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
-      <body>{children}</body>
+      <body>
+      <NextTopLoader />
+        {children}
+        </body>
     </html>
   );
 }

0f42746a01295701cb3ffaee18ae268448b2a472
CODILLION <163315001+codillion@users.noreply.github.com>
2024-03-17 06:38:57 +0000

    updatetoploader

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
       <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       <body>
-      <NextTopLoader />
+      <NextTopLoader color="#5f43ff" showSpinner={false} shadow="0 0 10px #5f43ff,0 0 5px #5f43ff"/>
         {children}
         </body>
     </html>
